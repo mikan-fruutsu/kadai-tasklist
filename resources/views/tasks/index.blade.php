fd28b969609cab1b2475df1d330d2c09f5e376a0
mikan.fruutsu <tkd.orangedotsl.000ve@gmail.com>
2020-08-22 00:47:03 +0000

    resubmit

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -5,11 +5,27 @@
    <h1>タスク一覧</h1>
 
     @if (count($tasks) > 0)
-        <ul>
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th>id</th>
+                    <th>一覧</th>
+                </tr>
+            </thead>
+            <tbody>
                 @foreach ($tasks as $task)
-                    <li>{{ $task->content }}</li>
+                <tr>
+                    <td>{{ $task->id }}</td>
+                    <td>{{ $task->content }}</td>
+                     
+                    <td>{!! link_to_route('tasks.show', $task->id, ['task' => $task->id]) !!}</td>
+                    <td>{{ $task->title }}</td>
+                    <td>{{ $task->content }}</td>
+                </tr>
                 @endforeach
-        </ul>    
+            </tbody>
+        </table>
+               
     @endif
     {!! link_to_route('tasks.create', '新規タスクの作成', [], ['class' => 'btn btn-primary']) !!}
 @endsection
